b94c2c94b79bfaaf5fb6cd4541bb0ba79eacc7b7
Y M Horn <ymh53r@gmail.com>
2025-08-27 16:54:01 +0300

    finished retriever

--- Retriever/main.py
+++ Retriever/main.py
@@ -1,10 +1,11 @@
 from kafka_pro import Produce
 from reciever import CollectionFetcher
+import pymongo
+import time
 
+kafka = Produce()
 
-a = Produce()
-
-b = CollectionFetcher('tweets')
+col = CollectionFetcher('tweets')
 
 def topic_name(doc):
     if doc['Antisemitic'] == 1:
@@ -12,7 +13,23 @@ def topic_name(doc):
     else:
         return 'raw_tweets_not_antisemitic'
 
-for document in b.collection.find():
-    print('a')
-    a.publish_message(topic=topic_name(document),message=str(document))
+def str_change(doc,field):
+    doc[field] = str(doc[field])
 
+def program_run_100_per_min(kafka_publisher,collection_fetch,sort_by,list_fields_str = None):
+    skip_amount = 0
+    while True:
+        count = 0
+        for document in collection_fetch.collection.find().sort(sort_by, pymongo.DESCENDING).limit(100).skip(skip_amount):
+            count += 1
+            if list_fields_str is not None:
+                for field in list_fields_str:
+                    str_change(document,field)
+            kafka_publisher.publish_message(topic=topic_name(document), message=document)
+        skip_amount += count
+        time.sleep(60)
+
+
+if __name__ == '__main__':
+
+    program_run_100_per_min(kafka,col,'CreateDate',['_id','CreateDate'])
