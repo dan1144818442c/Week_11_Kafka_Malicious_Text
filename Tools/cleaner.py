2394ea6ccd1b95f136353940939085dd1d4c1cec
d.c! <daniel.c99999@gmail.com>
2025-08-27 12:07:08 +0300

    commite tools

---
+++ Tools/cleaner.py
@@ -0,0 +1,35 @@
+import string
+import re
+import nltk
+
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+
+class Cleaner:
+    @staticmethod
+    def remove_special_characters_and_marks(text):
+        translator = str.maketrans('', '', string.punctuation)
+        return text.translate(translator)
+
+    @staticmethod
+    def removing_unnecessary_spaces(text):
+        return re.sub(' +', ' ', text).strip()
+
+    @staticmethod
+    def removing_stop_words(text ,language ='english'):
+        stop_words = set(nltk.corpus.stopwords.words(language))
+        text = ' '.join([word for word in text.split() if word not in (stop_words)])
+        return text
+
+    @staticmethod
+    def get_text_in_lowercase(text):
+        return text.lower()
+
+    @staticmethod
+    def get_matize_text(text):
+        lemmatizer =nltk.stem.WordNetLemmatizer()
+        word_list = nltk.word_tokenize(text)
+        lemmatized_output = ' '.join([lemmatizer.lemmatize(w) for w in word_list])
+        return  lemmatized_output
+
