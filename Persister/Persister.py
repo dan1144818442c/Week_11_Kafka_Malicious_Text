0cec2f199243a97f95cebfed234d63082f8f7d1c
d.c! <daniel.c99999@gmail.com>
2025-08-28 15:44:07 +0300

    update work project

--- Persister/Persister.py
+++
@@ -1,8 +0,0 @@
-from Tools import kafka_producer,kafka_consumer
-
-class Persistor:
-    def __init__(self , topic_a_to_listen, topic_b_to_listen):
-        self.con_a = kafka_consumer.Subscriber(topic_a_to_listen)
-        self.con_b = kafka_consumer.Subscriber(topic_b_to_listen)
-        self.pub = kafka_producer.Produce()
-
